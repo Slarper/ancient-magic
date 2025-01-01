0247e0ad7b7f35ddb725f311d17ea65b80135c06
Slarper <naivesimple@outlook.com>
2025-01-01 11:52:51 +0800

    API change: add hitresult != miss

--- src/main/java/com/example/IgniteSpell.java
+++ src/main/java/com/example/IgniteSpell.java
@@ -14,6 +14,7 @@ import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.EntityHitResult;
+import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
@@ -30,6 +31,10 @@ public class IgniteSpell {
             World world = caster.getWorld();
 
             for (var hitResult : hitResults){
+                if (hitResult.getType() == HitResult.Type.MISS) {
+                    break;
+                }
+
                 if (hitResult instanceof EntityHitResult
                 ){
                     var entity = ((EntityHitResult)hitResult).getEntity();
